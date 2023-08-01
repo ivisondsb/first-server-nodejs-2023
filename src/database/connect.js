75163761b2173214cd07367af01befb80449de41
Ivisondsb <Ivisonk0@gmail.com>
2023-08-01 20:57:08 -0300

    fix(connect): adjust database connect error

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectToDataBase = async () => {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/database?retryWrites=true&w=majority`,
-    (error) => {
-      if (error) {
-        return console.log("Error connecting to database", error);
-      }
-      return console.log("Database connection successful.");
-    }
-  );
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/database?retryWrites=true&w=majority`
+    );
+    console.log("Database connection successful.");
+  } catch (error) {
+    console.log("Error connecting to database:", error.message);
+  }
 };
 
 module.exports = connectToDataBase;
